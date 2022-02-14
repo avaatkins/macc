5849db7df647ba57c89db3ca71708d02fb2e2dad
avaatkins <99356366+avaatkins@users.noreply.github.com>
2022-02-14 11:39:13 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,80 @@
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+
+with_fx :bitcrusher do
+  stranger_things_theme
+end
+
+with_fx :ixi_techno do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :whammy do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :slicer do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
 
